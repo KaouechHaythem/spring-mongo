da1ab61e998fe6fec492bca59c57e26a42a6e9d0
Haythem Kaouech <haythemkaouech@gmail.com>
2023-11-15 16:43:03 +0100

    cors

--- src/main/java/TP/Devops/SpringMongoApp/SpringMongoAppApplication.java
+++ src/main/java/TP/Devops/SpringMongoApp/SpringMongoAppApplication.java
@@ -19,7 +19,7 @@ public class SpringMongoAppApplication {
 	public org.springframework.web.filter.CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("spring-mongo-frontend:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
