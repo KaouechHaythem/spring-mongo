8c3bb6d53c7db69a8de9908d4f004633c13f7e6e
Haythem Kaouech <haythemkaouech@gmail.com>
2023-11-25 16:30:52 +0100

    allow origin removed

--- src/main/java/TP/Devops/SpringMongoApp/SpringMongoAppApplication.java
+++ src/main/java/TP/Devops/SpringMongoApp/SpringMongoAppApplication.java
@@ -16,15 +16,15 @@ public class SpringMongoAppApplication {
 		SpringApplication.run(SpringMongoAppApplication.class, args);
 	}
 	@Bean
-	public org.springframework.web.filter.CorsFilter corsFilter() {
+	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("*","http://spring-boot-service.com/"));
-		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
-				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
+		corsConfiguration.setAllowedOrigins(Arrays.asList("*", "http://spring-boot-service.com"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Content-Type",
+				"Accept", "Authorization", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
 		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
-				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+				"Access-Control-Allow-Credentials"));
 		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
