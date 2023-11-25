4883c86b7bdb5e30da6692a1d8be7f9794a12dca
Haythem Kaouech <haythemkaouech@gmail.com>
2023-11-25 16:25:19 +0100

    one config

--- src/main/java/TP/Devops/SpringMongoApp/SpringMongoAppApplication.java
+++ src/main/java/TP/Devops/SpringMongoApp/SpringMongoAppApplication.java
@@ -19,7 +19,7 @@ public class SpringMongoAppApplication {
 	public org.springframework.web.filter.CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://20.88.170.197"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("*","http://spring-boot-service.com/"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
