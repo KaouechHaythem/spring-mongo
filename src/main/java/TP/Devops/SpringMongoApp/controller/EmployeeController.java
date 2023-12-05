2856ad406e0966d7e10d0db50321a9a91ba98145
Haythem Kaouech <haythemkaouech@gmail.com>
2023-12-05 19:16:53 +0100

    prom

--- src/main/java/TP/Devops/SpringMongoApp/controller/EmployeeController.java
+++ src/main/java/TP/Devops/SpringMongoApp/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package TP.Devops.SpringMongoApp.controller;
 
 import TP.Devops.SpringMongoApp.model.Employee;
 import TP.Devops.SpringMongoApp.service.EmployeeService;
+import io.micrometer.core.annotation.Timed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,9 +14,12 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/employee")
 @CrossOrigin(origins = {"http://www.spring-mongo.xyz","http://spring-mongo.xyz"})
+@Timed
+
 public class EmployeeController {
     @Autowired
     private  EmployeeService employeeService;
+
 
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
@@ -23,16 +27,21 @@ public class EmployeeController {
 
 
     @GetMapping("/all")
+    @Timed(value = "employee.getall.timer", description = "Time taken to get all  employees")
+
     public ResponseEntity<List<Employee>> getAllEmployees () {
         List<Employee> employees = employeeService.findAllEmployees();
         return new ResponseEntity<>(employees, HttpStatus.OK);
     }
 
     @GetMapping("/find/{id}")
+    @Timed(value = "employee.find.timer", description = "Time taken to find an employee")
+
     public ResponseEntity<Employee> getEmployeeById (@PathVariable("id") String id) {
         Employee employee = employeeService.findEmployeeById(id);
         return new ResponseEntity<>(employee, HttpStatus.OK);
     }
+    @Timed(value = "employee.add.timer", description = "Time taken to add an employee")
 
     @PostMapping("/add")
     public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
@@ -41,12 +50,16 @@ public class EmployeeController {
     }
 
     @PutMapping("/update")
+    @Timed(value = "employee.update.timer", description = "Time taken to update an employee")
+
     public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
         Employee updateEmployee = employeeService.updateEmployee(employee);
         return new ResponseEntity<>(updateEmployee, HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
+    @Timed(value = "employee.delete.timer", description = "Time taken to delete an employee")
+
     public ResponseEntity<?> deleteEmployee(@PathVariable("id") String id) {
         employeeService.deleteEmployee(id);
         return new ResponseEntity<>(HttpStatus.OK);
